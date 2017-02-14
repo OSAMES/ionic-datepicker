c11bf52a4d309b51e6af75cb4e3eaec4d6c8c017
Barbara Post <bpo@eonix.be>
2017-02-14 13:26:46 +0100

    Working on #1 : added all visual tests. Initial selection generally wrong, to debug and fix

--- test/www/js/controllers.js
+++ test/www/js/controllers.js
@@ -17,6 +17,19 @@ angular.module('starter.controllers', [])
 
       ionicDatePicker.openDatePicker(ipObj1);
     };
+      /* Monday first, day selection, popup */
+      $scope.openDatePickerStandardMonday = function() {
+        var ipObj1 = {
+          callback: function (val) {  //Mandatory
+            $scope.datepicker1 = new Date(val);
+          },
+          mondayFirst: true,          //Optional
+          closeOnSelect: false,       //Optional
+          templateType: 'popup'       //Optional
+        };
+
+        ionicDatePicker.openDatePicker(ipObj1);
+      };
       /* Sunday first, day selection, modal */
       $scope.openDatePickerStandardModal = function() {
         var ipObj1 = {
@@ -24,6 +37,19 @@ angular.module('starter.controllers', [])
             $scope.datepicker1 = new Date(val);
           },
           mondayFirst: false,          //Optional
+          closeOnSelect: false,       //Optional
+          templateType: 'modal'       //Optional
+        };
+
+        ionicDatePicker.openDatePicker(ipObj1);
+      };
+      /* Monday first, day selection, modal */
+      $scope.openDatePickerStandardModalMonday = function() {
+        var ipObj1 = {
+          callback: function (val) {  //Mandatory
+            $scope.datepicker1 = new Date(val);
+          },
+          mondayFirst: true,          //Optional
           closeOnSelect: false,       //Optional
           templateType: 'modal'       //Optional
         };
@@ -44,7 +70,20 @@ angular.module('starter.controllers', [])
 
         ionicDatePicker.openDatePicker(ipObj1);
       };
+      /* Monday first, week selection, popup */
+      $scope.openDatePickerWeekMonday = function() {
+        var ipObj1 = {
+          callback: function (val) {  //Mandatory
+            $scope.datepicker2 = new Date(val);
+          },
+          mondayFirst: true,          //Optional
+          closeOnSelect: false,       //Optional
+          templateType: 'popup',       //Optional
+          selectMode: 'week'
+        };
 
+        ionicDatePicker.openDatePicker(ipObj1);
+      };
       /* Sunday first, week selection, modal */
       $scope.openDatePickerWeekModal = function() {
         var ipObj1 = {
@@ -59,5 +98,60 @@ angular.module('starter.controllers', [])
 
         ionicDatePicker.openDatePicker(ipObj1);
       };
+      /* Sunday first, week selection, modal */
+      $scope.openDatePickerWeekModal = function() {
+        var ipObj1 = {
+          callback: function (val) {  //Mandatory
+            $scope.datepicker2 = new Date(val);
+          },
+          mondayFirst: false,          //Optional
+          closeOnSelect: false,       //Optional
+          templateType: 'modal',       //Optional
+          selectMode: 'week'
+        };
 
+        ionicDatePicker.openDatePicker(ipObj1);
+      };
+      /* Sunday first, week selection, modal */
+      $scope.openDatePickerWeekModal = function() {
+        var ipObj1 = {
+          callback: function (val) {  //Mandatory
+            $scope.datepicker2 = new Date(val);
+          },
+          mondayFirst: false,          //Optional
+          closeOnSelect: false,       //Optional
+          templateType: 'modal',       //Optional
+          selectMode: 'week'
+        };
+
+        ionicDatePicker.openDatePicker(ipObj1);
+      };
+      /* Sunday first, week selection, modal */
+      $scope.openDatePickerWeekModal = function() {
+        var ipObj1 = {
+          callback: function (val) {  //Mandatory
+            $scope.datepicker2 = new Date(val);
+          },
+          mondayFirst: false,          //Optional
+          closeOnSelect: false,       //Optional
+          templateType: 'modal',       //Optional
+          selectMode: 'week'
+        };
+
+        ionicDatePicker.openDatePicker(ipObj1);
+      };
+      /* Monday first, week selection, modal */
+      $scope.openDatePickerWeekModalMonday = function() {
+        var ipObj1 = {
+          callback: function (val) {  //Mandatory
+            $scope.datepicker2 = new Date(val);
+          },
+          mondayFirst: true,          //Optional
+          closeOnSelect: false,       //Optional
+          templateType: 'modal',       //Optional
+          selectMode: 'week'
+        };
+
+        ionicDatePicker.openDatePicker(ipObj1);
+      };
     }]);
